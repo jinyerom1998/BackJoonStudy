b72c0d6f1bf76f40e092afa5a1e673ea9e74fc93
Jin Yerom <125466588+jinyerom1998@users.noreply.github.com>
2024-09-23 09:17:37 +0900

    [level 0] Title: 배열 만들기 1, Time: 7.74 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList<Integer> arrList = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i % k == 0){
+                arrList.add(i);
+            }
+        }
+        int[] answer = new int[arrList.size()];
+        for(int i=0; i<arrList.size(); i++){
+            answer[i] = arrList.get(i).intValue();
+        }
+        return answer;
+    }
+}
