e60442036575f6ea2ec22a2be9b946de20126ad6
Jin Yerom <125466588+jinyerom1998@users.noreply.github.com>
2024-12-23 08:08:08 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sumOfDigits = 0;
+        int temp = x;
+        
+        while (temp > 0) {
+            sumOfDigits += temp % 10;
+            temp /= 10;
+        }
+        
+        return x % sumOfDigits == 0;
+    }
+}
